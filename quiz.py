1713573a84474aabf133be5cbdded1dafc3cd731
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-05-10 19:07:48 +0800

    Sets the foundation of the code

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 print("Welcome to Math Quiz!")
 print("Please select the type of quiz.")
@@ -7,8 +8,9 @@ print("Please select the type of quiz.")
 def main():
     equation = get_equation()
     length_of_equation = get_length()
+    number = get_number(length_of_equation)
 
-    print(equation, length_of_equation)
+    print(equation, length_of_equation, number)
 
 
 def get_equation():
@@ -25,10 +27,37 @@ def get_equation():
 def get_length():
     while True:
         try:
-            return int(input("Length of equation: "))
+            length = int(input("Length of equation (1, 2, 3): "))
+
+            if length == 1 or length == 2 or length == 3:
+                return length
+            
+            else:
+                print("Invalid Length")
+                
 
         except ValueError:
             print("Invalid number of length.")
+
+def get_number(length_of_equation):
+    if length_of_equation == 1:
+        x = random.randint(0, 9)
+        y = random.randint(0, 9)
+
+        return(x, y)
+    
+    elif length_of_equation == 2:
+        x = random.randint(00, 99)
+        y = random.randint(00, 99)
+
+        return(x, y)
+
+    elif length_of_equation == 3:
+        x = random.randint(000, 999)
+        y = random.randint(000, 999)
+
+        return(x, y)
+
 
 
 if __name__ == "__main__":
